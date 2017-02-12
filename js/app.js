a15ad300f395476c352b1b9999931ef391cb844d
Scoiatael85 <sdavis1985@gmail.com>
2017-02-11 23:47:07 -0500

    got second carousel working, backgorund meh

--- js/app.js
+++ js/app.js
@@ -1,3 +1,107 @@
+var game = {
+  "playersEOM" : [
+    {   
+    pc: "@BaerTaffy",
+    name: "Baer",
+    profilePic: "img/min/BaerTaffy.png", 
+    twitter: "https://twitter.com/BaerTaffy",
+    charNickname: "Ado",
+    charName: "Adotumal Milltall",
+    charImg: "img/min/Ado.png",
+    charRace: "Halfling",
+    charClass: "Bard",
+    id: "EOM0"
+    },
+    {   
+    pc: "@Strippin",
+    name: "Sam",
+    profilePic: "img/min/Strippin.jpg", 
+    twitter: "https://twitter.com/Strippin",
+    charNickname: "Rye",
+    charName: "Rye",
+    charImg: "img/min/Rye.png",
+    charRace: "Tiefling",
+    charClass: "Warlock",
+    id: "EOM1"
+    },
+    {   
+    pc: "@MargaretKrohn",
+    name: "Maggie",
+    profilePic: "img/min/MargaretKrohn.jpg", 
+    twitter: "https://twitter.com/MargaretKrohn",
+    charNickname: "Gromon",
+    charName: "Gromon",
+    charImg: "img/min/Gromon.png",
+    charRace: "Half-orc",
+    charClass: "Barbarian",
+    id: "EOM2"
+    },
+    {   
+    pc: "@MathasGames",
+    name: "Mathas",
+    profilePic: "img/min/MathasGames.jpg", 
+    twitter: "https://twitter.com/MathasGames",
+    charNickname: "Thonk",
+    charName: "Thonk",
+    charImg: "img/min/Thonk.png",
+    charRace: "Half-orc",
+    charClass: "Cleric",
+    id: "EOM3"
+    }
+  ],
+  "playersTF" : [
+      {   
+    pc: "@BaerTaffy",
+    name: "Baer",
+    profilePic: "img/min/BaerTaffy.png", 
+    twitter: "https://twitter.com/BaerTaffy",
+    charNickname: "Ado",
+    charName: "Adotumal Milltall",
+    charImg: "img/min/Ado.png",
+    charRace: "Halfling",
+    charClass: "Bard",
+    id: "TF0"
+    },
+    {   
+    pc: "@Strippin",
+    name: "Sam",
+    profilePic: "img/min/Strippin.jpg", 
+    twitter: "https://twitter.com/Strippin",
+    charNickname: "Rye",
+    charName: "Rye",
+    charImg: "img/min/Rye.png",
+    charRace: "Tiefling",
+    charClass: "Warlock",
+    id: "TF1"
+    },
+    {   
+    pc: "@MargaretKrohn",
+    name: "Maggie",
+    profilePic: "img/min/MargaretKrohn.jpg", 
+    twitter: "https://twitter.com/MargaretKrohn",
+    charNickname: "Gromon",
+    charName: "Gromon",
+    charImg: "img/min/Gromon.png",
+    charRace: "Half-orc",
+    charClass: "Barbarian",
+    id: "TF2"
+    },
+    {   
+    pc: "@MathasGames",
+    name: "Mathas",
+    profilePic: "img/min/MathasGames.jpg", 
+    twitter: "https://twitter.com/MathasGames",
+    charNickname: "Thonk",
+    charName: "Thonk",
+    charImg: "img/min/Thonk.png",
+    charRace: "Half-orc",
+    charClass: "Cleric",
+    id: "TF3"
+    }
+  ]
+};
+            
+
 // Iterator for number of times the pizzas in the background have scrolled.
 // Used by updatePositions() to decide when to log the average time per frame
 var frame = 0;
@@ -67,28 +171,66 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 $(document).ready(function(){
+    var game1Wheel = $('#game1Wheel');
+    var game2Wheel = $('#game2Wheel');
+    var EOMindicators = '<ol class="carousel-indicators"><li class="EOM0 active"></li><li class="EOM1"></li><li class="EOM2"></li><li class="EOM3"></li></ol>';
+    var TFindicators = '<ol class="carousel-indicators"><li class="TF0 active"></li><li class="TF1"></li><li class="TF2"></li><li class="TF3"></li></ol>';
+    var game1Meat = $('#game1Inner');
+    var game2Meat = $('#game2Inner');
+
+    game1Wheel.append(EOMindicators);
+    game2Wheel.append(TFindicators);
+
+    for (i= 0; i < game.playersEOM.length; i++) {
+      var wheelItem = '<div id="' + game.playersEOM[i].id + '" class="item"><div class="col-md-6"><img src="' 
+                      + game.playersEOM[i].profilePic + '" alt="' + game.playersEOM[i].name + '"><p><a href="' +
+                      game.playersEOM[i].twitter + '">' + game.playersEOM[i].pc 
+                      + '</a></p></div>' + '<div class="col-md-6"><img src="' + game.playersEOM[i].charImg + '" alt="' + game.playersEOM[i].charNickname +
+                      '"><p><span>Name:</span> ' + game.playersEOM[i].charName + '</p><p><span>Race:</span> ' + game.playersEOM[i].charRace + 
+                      '</p><p><span>Class:</span> ' + game.playersEOM[i].charClass + '</p></div></div>'
+      game1Meat.append(wheelItem);
+    };
+    for (i= 0; i < game.playersTF.length; i++) {
+      var wheel2Item = '<div id="' + game.playersTF[i].id + '" class="item"><div class="col-md-6"><img src="' 
+                      + game.playersTF[i].profilePic + '" alt="' + game.playersTF[i].name + '"><p><a href="' +
+                      game.playersTF[i].twitter + '">' + game.playersTF[i].pc 
+                      + '</a></p></div>' + '<div class="col-md-6"><img src="' + game.playersTF[i].charImg + '" alt="' + game.playersTF[i].charNickname +
+                      '"><p><span>Name:</span> ' + game.playersTF[i].charName + '</p><p><span>Race:</span> ' + game.playersTF[i].charRace + 
+                      '</p><p><span>Class:</span> ' + game.playersTF[i].charClass + '</p></div></div>'
+      game2Meat.append(wheel2Item);
+    };
+
+    $('#EOM0').addClass('active'); 
+    $('#TF0').addClass('active'); 
     // Activate Carousel
-    $("#myCarousel").carousel({interval: 2000});
+    $('.carousel').carousel({interval: 1500});
     
     // Enable Carousel Indicators
-    $(".item1").click(function(){
-        $("#myCarousel").carousel(0);
+    $(".EOM0").click(function(){
+        $("#game1Wheel").carousel(0);
     });
-    $(".item2").click(function(){
-        $("#myCarousel").carousel(1);
+    $(".EOM1").click(function(){
+        $("#game1Wheel").carousel(1);
     });
-    $(".item3").click(function(){
-        $("#myCarousel").carousel(2);
+    $(".EOM2").click(function(){
+        $("#game1Wheel").carousel(2);
     });
-    $(".item4").click(function(){
-        $("#myCarousel").carousel(3);
+    $(".EOM3").click(function(){
+        $("#game1Wheel").carousel(3);
     });
-    
-    // Enable Carousel Controls
-    $(".left").click(function(){
-        $("#myCarousel").carousel("prev");
+
+
+    // $("#game2Wheel").carousel({interval: 1750});
+    $(".TF0").click(function(){
+        $("#game2Wheel").carousel(0);
     });
-    $(".right").click(function(){
-        $("#myCarousel").carousel("next");
+    $(".TF1").click(function(){
+        $("#game2Wheel").carousel(1);
+    });
+    $(".TF2").click(function(){
+        $("#game2Wheel").carousel(2);
+    });
+    $(".TF3").click(function(){
+        $("#game2Wheel").carousel(3);
     });
 });
